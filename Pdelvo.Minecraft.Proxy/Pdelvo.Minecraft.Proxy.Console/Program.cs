3347c1f3c7573471d0bfa421646817e99b4589c5
Dennis Fischer <fischer_dennis@live.de>
2012-08-05 00:53:03 +0200

    Initial Commit

---
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Proxy.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
