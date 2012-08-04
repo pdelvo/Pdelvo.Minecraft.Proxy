0f6a4bc95c12a2829db54ce0087c05b44bdc1332
Dennis Fischer <fischer_dennis@live.de>
2012-08-05 01:12:25 +0200

    Started working on project structure
    Moved Minecraft Protocol Libraries in a different solution folder

---
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Library/IProxyServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Proxy.Library
+{
+    public interface IProxyServer : IDisposable
+    {
+        IPEndPoint LocalEndPoint { get; }
+        int ConnectedUsers { get; }
+        bool Listening { get; }
+        void Start();
+        Task StopAsync();
+    }
+}
---
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Library/ProxyServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Proxy.Library
+{
+    public class ProxyServer : IProxyServer
+    {
+
+        public IPEndPoint LocalEndPoint
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int ConnectedUsers
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Listening
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Dispose()
+        {
+
+        }
+    }
+}
