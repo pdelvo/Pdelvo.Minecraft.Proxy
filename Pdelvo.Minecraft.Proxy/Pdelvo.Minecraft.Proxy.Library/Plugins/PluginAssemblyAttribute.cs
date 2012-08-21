304877b1682d0ddc1d636df95cb699d6d836cf2c
Dennis Fischer <fischer_dennis@live.de>
2012-08-21 21:08:27 +0200

    Added PluginAssemblyAttribute documentation
    This is a part of #10

--- Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Library/Plugins/PluginAssemblyAttribute.cs
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Library/Plugins/PluginAssemblyAttribute.cs
@@ -7,12 +7,18 @@ using Pdelvo.Minecraft.Proxy.Library.Plugins;
 
 namespace Pdelvo.Minecraft.Proxy.Library.Plugins
 {
+    /// <summary>
+    /// A attribute which should be placed in  an assembly containing proxy server plugins to point the plugin loader where it can find plugins
+    /// </summary>
     [AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
     public sealed class PluginAssemblyAttribute : Attribute
     {
         readonly Type _pluginType;
 
-        // This is a positional argument
+        /// <summary>
+        /// Creates a new instance of the PluginAssemblyAttribute
+        /// </summary>
+        /// <param name="pluginType">A type which must inherit from <see cref="PluginBase">PluginBase</see></param>
         public PluginAssemblyAttribute(Type pluginType)
         {
             if(pluginType == null)throw new ArgumentNullException("pluginType");
@@ -21,6 +27,9 @@ namespace Pdelvo.Minecraft.Proxy.Library.Plugins
             _pluginType = pluginType;
         }
 
+        /// <summary>
+        /// The type of the plugin this attribute points at
+        /// </summary>
         public Type PluginType
         {
             get { return _pluginType; }
