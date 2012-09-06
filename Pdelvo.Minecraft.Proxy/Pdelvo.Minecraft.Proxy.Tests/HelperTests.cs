15bd4b1654176fa61d4d89212a5123f97de84495
Dennis Fischer <fischer_dennis@live.de>
2012-09-06 22:45:30 +0200

    Renamed unit tests
    Naming them to what they actually test is more useful

--- Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Tests/HelperTests.cs
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Tests/HelperTests.cs
@@ -9,22 +9,36 @@ namespace Pdelvo.Minecraft.Proxy.Tests
     public class HelperTests
     {
         [TestMethod]
-        public void SessionTest()
+        public void VerifySessionGeneratesRandomOutput()
         {
-            var hash = Session.GetSessionHash();
+            for (int i = 0; i < 10; i++)
+            {
+                var hash = Session.GetSessionHash();
 
-            var hash2 = Session.GetSessionHash();
+                var hash2 = Session.GetSessionHash();
 
-            Assert.AreNotEqual(hash, hash2);
-
-            var lng = Convert.ToInt64(hash, 16);
-            var lng2 = Convert.ToInt64(hash2, 16);
-
-            Assert.IsTrue(lng >= 0);
-            Assert.IsTrue(lng2 >= 0);
+                Assert.AreNotEqual(hash, hash2);
+            }
         }
+
         [TestMethod]
-        public async Task UserAccountTest()
+        public void VerifySessionGeneratesCorrectOutput()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                var hash = Session.GetSessionHash();
+
+                var hash2 = Session.GetSessionHash();
+                var lng = Convert.ToInt64(hash, 16);
+                var lng2 = Convert.ToInt64(hash2, 16);
+
+                Assert.IsTrue(lng >= 0);
+                Assert.IsTrue(lng2 >= 0);
+            }
+        }
+
+        [TestMethod]
+        public async Task VerifyUserAccountCheckFailsOnWrongDetails()
         {
             Assert.IsFalse(await UserAccountServices.CheckAccountAsync("thisdoesnotexist", "acoolhash"));
         }
