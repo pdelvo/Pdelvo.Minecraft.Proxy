b04d0ed517f4af8e773f535bbd9a3517e4c4dbe1
Dennis Fischer <fischer_dennis@live.de>
2012-09-06 22:38:38 +0200

    Added unit tests
    Added some unit tests. Targets #29

---
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Tests/HelperTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pdelvo.Minecraft.Proxy.Library;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Proxy.Tests
+{
+    [TestClass]
+    public class HelperTests
+    {
+        [TestMethod]
+        public void SessionTest()
+        {
+            var hash = Session.GetSessionHash();
+
+            var hash2 = Session.GetSessionHash();
+
+            Assert.AreNotEqual(hash, hash2);
+
+            var lng = Convert.ToInt64(hash, 16);
+            var lng2 = Convert.ToInt64(hash2, 16);
+
+            Assert.IsTrue(lng >= 0);
+            Assert.IsTrue(lng2 >= 0);
+        }
+        [TestMethod]
+        public async Task UserAccountTest()
+        {
+            Assert.IsFalse(await UserAccountServices.CheckAccountAsync("thisdoesnotexist", "acoolhash"));
+        }
+    }
+}
