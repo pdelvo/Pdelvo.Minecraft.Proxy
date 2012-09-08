566ad79c8af4d2bf38f21ee8667cc1df37eb407f
Dennis Fischer <fischer_dennis@live.de>
2012-09-08 10:28:15 +0200

    Started working on windows service
    Does not work yet

---
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Service/ProxyService.cs
@@ -0,0 +1,38 @@
+﻿using Pdelvo.Minecraft.Proxy.Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdelvo.Minecraft.Proxy.Service
+{
+    public partial class ProxyService : ServiceBase
+    {
+        IProxyServer _server;
+        public ProxyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            _server = new ProxyServer();
+            _server.Start();
+
+            while (System.Console.ReadKey(true).Key != ConsoleKey.Q) { }
+        }
+
+        protected override async void OnStop()
+        {
+            await _server.StopAsync();
+
+            base.OnStop();
+        }
+    }
+}
