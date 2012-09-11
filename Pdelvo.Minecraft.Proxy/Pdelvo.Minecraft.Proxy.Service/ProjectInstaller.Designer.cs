69f2937ec26fe5da6f7efbc5cfe87a3b6f1b6116
Dennis Fischer <fischer_dennis@live.de>
2012-09-11 18:15:00 +0200

    Add working installer

--- Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Service/ProjectInstaller.Designer.cs
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Service/ProjectInstaller.Designer.cs
@@ -39,14 +39,15 @@
             // 
             // serviceProcessInstaller1
             // 
-            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.NetworkService;
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
             this.serviceProcessInstaller1.Password = null;
             this.serviceProcessInstaller1.Username = null;
             // 
             // ProjectInstaller
             // 
             this.Installers.AddRange(new System.Configuration.Install.Installer[] {
-            this.serviceInstaller1});
+            this.serviceInstaller1,
+            this.serviceProcessInstaller1});
 
         }
 
