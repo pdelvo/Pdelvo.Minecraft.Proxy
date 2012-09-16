8d425159065188d6e54fb1fabe109eef6c881adc
Dennis Fischer <fischer_dennis@live.de>
2012-09-16 14:31:29 +0200

    Added unhandled exception logging
    I hope this helps finding the bug in #32

--- Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Console/Program.cs
+++ Pdelvo.Minecraft.Proxy/Pdelvo.Minecraft.Proxy.Console/Program.cs
@@ -6,20 +6,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Pdelvo.Minecraft.Proxy.Library;
 using Pdelvo.Minecraft.Proxy.Library.Plugins;
+using log4net;
 
 namespace Pdelvo.Minecraft.Proxy.Console
 {
     class Program
     {
+        static ILog _logger = LogManager.GetLogger("Program logger");
         static void Main(string[] args)
         {
             System.Console.WriteLine("Press q to quit");
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             log4net.Config.XmlConfigurator.Configure();
             IProxyServer server = new ProxyServer();
             server.Start();
 
             while (System.Console.ReadKey(true).Key != ConsoleKey.Q) { }
             server.StopAsync();
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger.Fatal("Fatal exception occured.", (Exception)e.ExceptionObject);
         }
     }
 }
